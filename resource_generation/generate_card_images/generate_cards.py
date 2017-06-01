8b0c6b85b142fa0b3d149bf81f1b1a5e6e059601
rook <HalfEllipsis@openaliasbox.org>
2017-05-31 22:03:44 -0400

    Added face card image ability. Needs sophistacation

--- resource_generation/generate_card_images/generate_cards.py
+++ resource_generation/generate_card_images/generate_cards.py
@@ -13,7 +13,8 @@ total_size = (cdef['dimensions']['size'][0]*number_of_cards,cdef['dimensions']['
 card_stock = Image.open(cdef['card_stock']['file'])
 card_stock = card_stock.crop(cdef['card_stock']['crop_offset']+cdef['dimensions']['size'])
 
-card_cutout = custom_geo.gen_rounded_rectangle(cdef['dimensions']['size'],cdef['dimensions']['radius'],'RGBA',(0,0,0,255))
+opaque = (0,0,0,255)
+card_cutout = custom_geo.gen_rounded_rectangle(cdef['dimensions']['size'],cdef['dimensions']['corner_round_radius'],'RGBA',opaque)
 
 bg_image = Image.new('RGBA',cdef['dimensions']['size'],cdef['bg_color'])
 
@@ -23,22 +24,27 @@ for suit in cdef['suits'].keys():
         
         for card_index,card in enumerate(cdef['cards'].keys()):
             card_image = card_stock.copy()
-            effective_locations = []
-            for location in cdef['cards'][card]['pips']:
-                effective_locations.append(location)
-            for mirror in cdef['cards'][card]['mirrors']:
-                new_locations = []
+            
+            if cdef['cards'][card]['is_face']:
+                face_image = Image.open(cdef['suits'][suit]['faces'][card]['image'])
+                card_image.paste(face_image,box=(0,0),mask=face_image)
+            else:#pip based image
+                effective_locations = []
+                for location in cdef['cards'][card]['pips']:
+                    effective_locations.append(location)
+                for mirror in cdef['cards'][card]['mirrors']:
+                    new_locations = []
+                    for location in effective_locations:
+                        center_anchored_location = custom_geo.center_anchor(location,(1,1))
+                        center_anchored_mirrored_location = custom_geo.mirror(center_anchored_location,mirror)
+                        mirrored_location = custom_geo.center_unanchor(center_anchored_mirrored_location,(1,1))
+                        if not custom_geo.too_close(location,mirrored_location,0.01):
+                            new_locations.append(mirrored_location)
+                    effective_locations.extend(new_locations)
                 for location in effective_locations:
-                    center_anchored_location = custom_geo.center_anchor(location,(1,1))
-                    center_anchored_mirrored_location = custom_geo.mirror(center_anchored_location,mirror)
-                    mirrored_location = custom_geo.center_unanchor(center_anchored_mirrored_location,(1,1))
-                    if not custom_geo.too_close(location,mirrored_location,0.01):
-                        new_locations.append(mirrored_location)
-                effective_locations.extend(new_locations)
-            for location in effective_locations:
-                location_scaled = custom_geo.center_anchor(custom_geo.relative_location(location,card_image.size),pip_image.size)
-                location_scaled_int = custom_geo.makeint(location_scaled)
-                card_image.paste(pip_image,box=location_scaled_int,mask=pip_image)
+                    location_scaled = custom_geo.center_anchor(custom_geo.relative_location(location,card_image.size),pip_image.size)
+                    location_scaled_int = custom_geo.makeint(location_scaled)
+                    card_image.paste(pip_image,box=location_scaled_int,mask=pip_image)
                     
             finished_card_image = Image.composite(card_image,bg_image,card_cutout)
             location_in_sheet = (card_index*card_stock.size[0],0)
