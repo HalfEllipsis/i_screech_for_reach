2463a8b7643495149e18f51af07c0d98ad07bd24
rook <HalfEllipsis@openaliasbox.org>
2017-06-07 22:57:08 -0400

    Mirroring of card value logo finished

--- resource_generation/generate_card_images/generate_cards.py
+++ resource_generation/generate_card_images/generate_cards.py
@@ -44,15 +44,27 @@ for suit in cdef['suits'].keys():
                             new_locations.append(mirrored_location)
                     effective_locations.extend(new_locations)
                 for location in effective_locations:
-                    location_scaled = custom_geo.center_anchor(custom_geo.relative_location(location,card_image.size),pip_image.size)
-                    location_scaled_int = custom_geo.makeint(location_scaled)
-                    card_image.paste(pip_image,box=location_scaled_int,mask=pip_image)
+                    location_scaled = custom_geo.relative_location(location,card_image.size)
+                    location_scaled_top_left = custom_geo.center_anchor(location_scaled,pip_image.size)
+                    location_scaled_top_left_int = custom_geo.makeint(location_scaled_top_left)
+                    card_image.paste(pip_image,box=location_scaled_top_left_int,mask=pip_image)
             
             logo_size = custom_geo.makeint(custom_geo.relative_size(cdef['logo']['size'],cdef['dimensions']['size']))
             logo_image = Image.new('RGBA',logo_size,translucence)
             logo_draw = ImageDraw.Draw(logo_image)
-            logo_positon = custom_geo.relative_location(cdef['logo']['position'],cdef['dimensions']['size'])
-            logo_positon_center_anchored = custom_geo.makeint(custom_geo.center_anchor(logo_positon,logo_size))
+            
+            logo_location = cdef['logo']['position']
+            logo_location_center_anchored = custom_geo.center_anchor(logo_location,(1,1))
+            logo_location_mirrored_center_anchored = custom_geo.mirror(logo_location_center_anchored,(1,-1))
+            logo_location_mirrored = custom_geo.center_unanchor(logo_location_mirrored_center_anchored,(1,1))
+            
+            logo_location_scaled = custom_geo.relative_location(logo_location,cdef['dimensions']['size'])
+            logo_location_scaled_top_left = custom_geo.center_anchor(logo_location_scaled,logo_size)
+            logo_location_scaled_top_left_int = custom_geo.makeint(logo_location_scaled_top_left)
+            
+            logo_location_mirrored_scaled = custom_geo.relative_location(logo_location_mirrored,cdef['dimensions']['size'])
+            logo_location_mirrored_scaled_top_left = custom_geo.center_anchor(logo_location_mirrored_scaled,logo_size)
+            logo_location_mirrored_scaled_top_left_int = custom_geo.makeint(logo_location_mirrored_scaled_top_left)            
             
             font_height = logo_size[1]*cdef['logo']['text']['size']
             font_position = custom_geo.makeint(custom_geo.relative_location(cdef['logo']['text']['position'],logo_size))
@@ -63,12 +75,15 @@ for suit in cdef['suits'].keys():
             logo_symbol_size = custom_geo.makeint(custom_geo.fit_size(logo_symbol.size,logo_size,cdef['logo']['symbol']['size']))
             logo_symbol_resized = logo_symbol.resize(logo_symbol_size,resample=Image.LANCZOS)
             logo_symbol_position =  custom_geo.relative_location(cdef['logo']['symbol']['position'],logo_symbol_size)
-            logo_symbol_position_center_anchored = custom_geo.makeint(custom_geo.center_anchor(logo_symbol_position,logo_symbol_resized.size))
+            logo_symbol_position_top_left = custom_geo.makeint(custom_geo.center_anchor(logo_symbol_position,logo_symbol_resized.size))
             
-            logo_image.paste(logo_symbol_resized,box=logo_symbol_position_center_anchored,mask=logo_symbol_resized)
+            logo_image.paste(logo_symbol_resized,box=logo_symbol_position_top_left,mask=logo_symbol_resized)
             logo_draw.text(font_position,card,font=font,fill=font_color)
-            card_image.paste(logo_image,box=logo_positon_center_anchored,mask=logo_image)
+            logo_image_flipped = logo_image.transpose(Image.ROTATE_180)
             
+            card_image.paste(logo_image,box=logo_location_scaled_top_left_int,mask=logo_image)
+            card_image.paste(logo_image_flipped,box=logo_location_mirrored_scaled_top_left_int,mask=logo_image_flipped)    
+                 
             finished_card_image = Image.composite(card_image,bg_image,card_cutout)
             location_in_sheet = (card_index*card_stock.size[0],0)
             card_sheet_image.paste(finished_card_image,box=location_in_sheet,mask=finished_card_image)
